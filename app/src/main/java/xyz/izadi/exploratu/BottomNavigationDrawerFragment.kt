0fa21e135f624a4ca017a78e4cdc533e4d759de1
Izadi Egizabal Alkorta <64izadi@gmail.com>
2019-05-15 00:12:53 +0200

    Basic project creation

---
+++ app/src/main/java/xyz/izadi/exploratu/BottomNavigationDrawerFragment.kt
@@ -0,0 +1,28 @@
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.fragment_bottom_navigation_drawer.*
+import xyz.izadi.exploratu.R
+
+class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_bottom_navigation_drawer, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        navigation_view.setNavigationItemSelectedListener { menuItem ->
+            // Bottom Navigation Drawer menu item clicks
+            when (menuItem.itemId) {
+                 // R.id.nav1 -> context!!.toast(getString(R.string.nav1_clicked))
+            }
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+            true
+        }
+    }
+}
---
+++ app/src/main/java/xyz/izadi/exploratu/MainActivity.kt
@@ -0,0 +1,35 @@
+package xyz.izadi.exploratu
+
+import BottomNavigationDrawerFragment
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(bottomAppBar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_bottom_app_bar, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            android.R.id.home -> {
+                val bottomNavDrawerFragment = BottomNavigationDrawerFragment()
+                bottomNavDrawerFragment.show(supportFragmentManager, bottomNavDrawerFragment.tag)
+            }
+        }
+        return true
+    }
+}
